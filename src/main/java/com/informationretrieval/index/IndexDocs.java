fb20c948f2057b537d09b1d1abcaa2c3563a274b
Joseph <josephodr@hotmail.com>
2018-10-20 15:48:22 +0100

    Removed unused imports

--- src/main/java/com/informationretrieval/index/IndexDocs.java
+++ src/main/java/com/informationretrieval/index/IndexDocs.java
@@ -11,7 +11,6 @@ import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
